e77299b6121728312b9c7cde84caca7092d26831
Ivan Garcia <ivanvlora@gmail.com>
2018-10-01 22:31:21 +0900

    Created API post for Users

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const auth = require("./routes/api/auth");
+const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -26,7 +31,7 @@ app.get("/", (req, res) => {
 });
 
 // Use Routes
-app.use("/api/auth", auth);
+app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
