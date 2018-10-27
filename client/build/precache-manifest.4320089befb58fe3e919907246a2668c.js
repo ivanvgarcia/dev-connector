ebde9d29ba7221673a5a2464c1d58c82a8c3963d
Ivan Garcia <ivanvlora@gmail.com>
2018-10-28 08:13:24 +0900

    edit gitignore

---
+++ client/build/precache-manifest.4320089befb58fe3e919907246a2668c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "786d8c165d2c19128599dfd44a003ff1",
+    "url": "/static/media/showcase.786d8c16.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da2ad08943e25d09a67e",
+    "url": "/static/js/main.da2ad089.chunk.js"
+  },
+  {
+    "revision": "9fd738b53d49b7d83bf3",
+    "url": "/static/js/1.9fd738b5.chunk.js"
+  },
+  {
+    "revision": "da2ad08943e25d09a67e",
+    "url": "/static/css/main.73c25fd9.chunk.css"
+  },
+  {
+    "revision": "2f26eee27d2bf5cedaaf1cdd713e88e9",
+    "url": "/index.html"
+  }
+];
